ab710de3cd0bda99f89097a6d57c9a11f7460b70
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-06-03 14:23:27 +0300

    feat: add tests for main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,17 @@
+from pages.main_page import MainPage
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 def test_guest_can_go_to_login_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser: WebDriver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
